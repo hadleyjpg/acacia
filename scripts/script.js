396be3bc86f4ec0f6d248b607abac5e66d15e830
Hadley Slucher <hadleyslucher@gmail.com>
2020-07-07 21:43:22 -0400

    Smooth scroll, hamburger menu added

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav');
+
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+});
